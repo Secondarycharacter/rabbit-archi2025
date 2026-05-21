c6e4ec9046c5e929ae96b6c2f2914c3197142c92
rabbit3419 <rabbit3419@gmail.com>
2026-05-21 22:50:40 +0900

    Update roadmap.js

--- roadmap/roadmap.js
+++ roadmap/roadmap.js
@@ -17,11 +17,11 @@ const COLLECTION = 'projects';
 
 const defaultProjects = [
   { type: 'type1', date: '2025.11', period: '2025 H2', desc: ['서충주 종합사회복지관', '건립사업 설계공모'], rank: '4위' },
-  { type: 'type1', date: '2026.01', period: '2026 H1', desc: ['진주시 글로벌', '어울림센터 조성사업', '설계공모'], rank: '5위' },
-  { type: 'type2', date: '2026.02', period: '2026 H1', desc: ['가평OO수녀원', '북원도 작성'], rank: '' },
-  { type: 'type1', date: '2026.03', period: '2026 H1', desc: ['거창군 지역활력타운', '다부리복합문화센터', '건립사업 설계공모'], rank: '10위' },
-  { type: 'type1', date: '2026.05', period: '2026 H1', desc: ['앵기발골 시니어형', '소규모체육관 건립사업', '설계공모'], rank: '4위' },
-  { type: 'type2', date: '2026.05', period: '2026 H1', desc: ['가평OO수녀원', '기본 및 실시설계', '진행중...'], rank: '' }
+  { type: 'type1', date: '2026.01', period: '2026 H1', desc: ['진주시 글로컬', '어울림센터 조성사업', '설계공모'], rank: '5위' },
+  { type: 'type2', date: '2026.02', period: '2026 H1', desc: ['가평OO 수녀원', '복원도 작성'], rank: '' },
+  { type: 'type1', date: '2026.03', period: '2026 H1', desc: ['거창군 지역활력타운', '다누리복합문화센터', '건립사업 설계공모'], rank: '10위' },
+  { type: 'type1', date: '2026.05', period: '2026 H1', desc: ['앵지발골 시니어형', '소규모체육관 건립사업', '설계공모'], rank: '4위' },
+  { type: 'type2', date: '2026.05', period: '2026 H1', desc: ['가평OO 수녀원', '기본 및 실시설계', '진행중...'], rank: '' }
 ];
 
 async function loadProjectsFromFirestore() {
